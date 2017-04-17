e963f0b79692858ceee6b679f917302f380742c5
ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>
2017-04-17 09:14:04 +0000

    table: batch ipv4 withdrawal messages
    Signed-off-by: ISHIDA Wataru <ishida.wataru@lab.ntt.co.jp>

--- server/bmp.go
+++ server/bmp.go
@@ -17,14 +17,16 @@ package server
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"strconv"
+	"time"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/osrg/gobgp/config"
 	"github.com/osrg/gobgp/packet/bgp"
 	"github.com/osrg/gobgp/packet/bmp"
 	"github.com/osrg/gobgp/table"
-	"net"
-	"strconv"
-	"time"
 )
 
 type ribout map[string][]*table.Path
@@ -153,7 +155,20 @@ func (b *bmpClient) loop() {
 									pathList = append(pathList, p)
 								}
 							}
-							for _, u := range table.CreateUpdateMsgFromPaths(pathList) {
+							msgs, g := table.CreateUpdateMsgFromPaths(pathList)
+							for _, u := range msgs {
+								payload, _ := u.Serialize()
+								if err := write(bmpPeerRoute(bmp.BMP_PEER_TYPE_GLOBAL, msg.PostPolicy, 0, info, msg.Timestamp.Unix(), payload)); err != nil {
+									return false
+								}
+							}
+							for {
+								u, err := g.Next()
+								if err == io.EOF {
+									break
+								} else if err != nil {
+									return false
+								}
 								payload, _ := u.Serialize()
 								if err := write(bmpPeerRoute(bmp.BMP_PEER_TYPE_GLOBAL, msg.PostPolicy, 0, info, msg.Timestamp.Unix(), payload)); err != nil {
 									return false
